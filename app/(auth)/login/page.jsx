61e1cd4cbfe2cec15eac1e19620e72980ea48ee7
Cryptid <09akshatmishra@gmail.com>
2024-05-04 13:07:12 +0530

    lc

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,18 +1,14 @@
-import LoginForm from "@/components/loginForm";
-import { handleGithubLogin } from "@/lib/action";
-import styles from "./login.module.css";
+import React from 'react'
+import LoginForm from '@/app/components/loginForm';
 
 const LoginPage = () => {
 
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <form action={handleGithubLogin}>
-          <button className={styles.github}>Login with Github</button>
-        </form>
-        <LoginForm />
+    
+        <div>
+        <LoginForm/>
       </div>
-    </div>
+  
   );
 };
 
